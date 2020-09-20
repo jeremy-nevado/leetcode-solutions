1dbecebe103238986ec4a17ba8004c52192412f9
jeremy-nevado <jeremynevado@gmail.com>
2020-09-20 19:03:26 -0400

    Updated invert binary tree question and updated two-sum question.

--- python/easy-python/invert-binary-tree.py
+++ python/easy-python/invert-binary-tree.py
@@ -1,16 +1,16 @@
-import unittest
-
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-        
+
+
+
 class Solution:
     def invertTree(self, root):
-        if root == None:
+        if root is None:
             return
-        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        root.left = self.invertTree(root.right)
+        root.right = self.invertTree(root.left)
+
         return root
-
-
---
+++ rust/easy-rust/two-sum/target/debug/deps/two_sum-90462c509b161b84.d
@@ -0,0 +1,5 @@
+/home/jeremy/Coding/leetcode-solutions/rust/easy-rust/two-sum/target/debug/deps/two_sum-90462c509b161b84.rmeta: src/main.rs
+
+/home/jeremy/Coding/leetcode-solutions/rust/easy-rust/two-sum/target/debug/deps/two_sum-90462c509b161b84.d: src/main.rs
+
+src/main.rs:
